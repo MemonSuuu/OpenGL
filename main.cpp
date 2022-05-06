bb2ff7a4220f1c0590c44f8600ab5976b4d9dd37
Ramon Costa <rrcostapatel@gmail.com>
2022-05-06 13:58:58 -0700

    modified

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 //standard headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
 #include <GL/glew.h>
 #include <glfw/glfw3.h>
+
+class VertexBuffer {
+    private:
+    //identify vertex buffer
+        GLuint vertexbuffer;
+    public:
+        VertexBuffer(const std::vector<float> &vertices){
+            // Generate 1 buffer, put the resulting identifier in vertexbuffer
+            glGenBuffers(1, &vertexbuffer);
+            // The following commands will talk about our 'vertexbuffer' buffer
+            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            // Give our vertices to OpenGL.
+            glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), vertices.data(), GL_STATIC_DRAW);
+        }
+        GLuint getBuffer(){
+            return vertexbuffer;
+        }
+        ~VertexBuffer(){
+            glDeleteBuffers(1, &vertexbuffer);
+        }
+};
+
+class VAO{
+    private:
+        GLuint VertexArrayID;
+    public:
+        VAO(){
+            //create a VAO
+            glGenVertexArrays(1, &VertexArrayID);
+            glBindVertexArray(VertexArrayID);
+        }
+        GLuint getID(){
+            return VertexArrayID;
+        }
+};
 
 int main(){
     //initialize glfw
@@ -39,43 +75,35 @@ int main(){
         return -1;
     }
 
+    VAO vao{};
+
+    static const std::vector<GLfloat> vertices {
+            -.50f, -1.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            0.50f,  1.0f, 0.0f
+    };
+    
+    VertexBuffer vB(vertices);
+    
+    // 1st attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride (for other data)
+        (void*)0            // array buffer offset (other data)
+    );
+
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     do{
         // Clear the screen
         glClear( GL_COLOR_BUFFER_BIT );
 
-        // Draw
-        GLuint VertexArrayID;
-        glGenVertexArrays(1, &VertexArrayID);
-        glBindVertexArray(VertexArrayID);
-
-        static const GLfloat vertices[] = {
-            -.50f, -1.0f, 0.0f,
-            1.0f, 0.0f, 0.0f,
-            0.50f,  1.0f, 0.0f
-        };
-        
-        // This will identify our vertex buffer
-        GLuint vertexbuffer;
-        // Generate 1 buffer, put the resulting identifier in vertexbuffer
-        glGenBuffers(1, &vertexbuffer);
-        // The following commands will talk about our 'vertexbuffer' buffer
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        // Give our vertices to OpenGL.
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-        // 1st attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(
-            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-            3,                  // size
-            GL_FLOAT,           // type
-            GL_FALSE,           // normalized?
-            0,                  // stride
-            (void*)0            // array buffer offset
-        );
         // Draw the triangle !
+        glBindVertexArray(vao.getID());
         glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
         glDisableVertexAttribArray(0);
         
@@ -85,4 +113,5 @@ int main(){
 
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
+
 }
